f4656ad3935d86a9c97708708605e043be9b7c23
LlopiU <llopiulisesalan10@gmail.com>
2023-07-31 23:07:31 -0300

    final de tailkwind

--- Desarrollo-Frontend/.next/static/chunks/webpack.js
+++ Desarrollo-Frontend/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2798ab9fbec38645"; }
+/******/ 		__webpack_require__.h = function() { return "d0c8beb99ec6780f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
